53b9476932cc5ab9086a47c928ef0634ed5e486c
Tano Paul <tanopaul38@gmail.com>
2023-07-31 22:58:55 -0400

    completed phase 1 scope lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'bobby'
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'jim';
+}
